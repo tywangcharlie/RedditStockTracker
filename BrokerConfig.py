349a3fd1b365a554223ea874972af302d7e5ef15
tywangcharlie <tywang.charlie@gmail.com>
2021-02-14 22:50:16 -0600

    add reddit stock tracker

--- BrokerConfig.py
+++
@@ -1,3 +0,0 @@
-API_KEY = 'AKFQTPVGJ6URHOW9H496'
-API_SECRET = 'NLN8RfbRWmeJQLEnbskswI8q1aHgjWAgwN2wYAGj'
-API_URL = 'https://api.alpaca.markets'
--- DBConfig.py
+++
@@ -1,4 +0,0 @@
-DB_HOST = 'localhost'
-DB_USER = 'tywang'
-DB_PASS = '2904'
-DB_NAME = 'redditdb'
